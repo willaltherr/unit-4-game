8ffaafab409f3492c6918b46c6251234b4c9bfe5
Wills Grave <will@keydigitalconsulting.com>
2019-08-30 17:44:17 -0400

    Added the pseudo code to the javascript file.

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,3 +1,18 @@
 // Pseudo Coding
 
-// 
+// Each crystal needs a ranom number between 1-12
+
+// New Random number that we have to target between 19-120
+
+// Any crystal that gets click should add to the total results
+
+// Tally the wins and losses
+
+// If the results equal, win by 1 increment
+
+// If the result goes over the Random target number, lose by 1 increment
+
+// Start over once you either win or lose
+
+
+
